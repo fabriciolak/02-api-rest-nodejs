d706f4dd8ea437a89cdf1eb823af91cb09ca5ac8
Fabrício <fabriciolak12@gmail.com>
2023-03-27 22:30:05 -0300

    configurando um hook global

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,14 @@
-import { randomUUID } from 'crypto'
+import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 import { checkIfSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', async (request, reply) => {
+    console.log(`[${request.method}] ${request.url}`)
+  })
+
   app.get(
     '/',
     {
